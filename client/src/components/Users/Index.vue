f0a63aac958faedaad87ba8678450d4338c201f8
pakawat <pakawatkut@gmail.com>
2019-12-30 15:27:50 +0700

    add User Api , create and data()

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -1,15 +1,28 @@
 <template>
 	<div>
 		<h1>Get All User</h1>
-	</div>
+		<h4>จำนวนผู้ใช้งาน {{users.length}}</h4>
+			<div v-for="user in users" v-bind:key="user.id">
+				<p>id : {{user.id}}</p>
+				<p>ชื่อ-นามสกุล : {{user.name}} - {{user.lastname}}</p>
+				<p>email : {{user.email}}</p>
+				<p>password : {{user.password}}</p>
+			</div>		
+		</div>
 </template>
 
 <script>
 	import UsersService from '@/services/UsersService'
 	export default {
+		data(){
+			return {
+				users : []
+			}
+		},
 		async created(){
-			let results = (await UsersService.index()).data
-			console.log(results)
+			// let results = (await UsersService.index()).data
+			// console.log(results)
+			this.users = (await UsersService.index()).data
 		}
 	}
 </script>
