5f765c77659ef56df5adff8bd62ec865ab261a37
pakawat <pakawatkut@gmail.com>
2020-01-06 18:04:38 +0700

    Back-end set routes isAuthenController /users done

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,12 @@
 // routes.js
 const UserController = require('./controllers/UserController')
 const UserAuthenController = require('./controllers/UserAuthenController')
+const isAuthenController = require('./authen/isAuthenController')
 module.exports = (app) => {
 	// RestFul Api for users menagement
-	app.get('/users',UserController.index)
+	app.get('/users',
+		isAuthenController,
+		UserController.index)
 	app.get('/user/:userId',UserController.show)
 	app.post('/user',UserController.create)
 	app.put('/user/:userId',UserController.put)
