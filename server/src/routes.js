ef6f5514e26ed5eec503e0b77c27ae8f8f5e846a
pakawat <pakawatkut@gmail.com>
2020-01-30 00:37:19 +0700

    upload utils

--- server/src/routes.js
+++ server/src/routes.js
@@ -13,45 +13,48 @@ const multer = require('multer')
 module.exports = (app) => {
 
 	// upload section 
-	let storage = multer.diskStorage({
-		destination:function(req,file,callback){
-			callback(null,'./public/uploads')
-		},
-		filename : function(req,file,callback){
-			// callback(null,file.name + '-' + Date.now())
-			console.log(file)
-			callback(null,file.originalname)
-		}
-	})
-}
-let upload = multer({storage:storage}).array("userPhoto",10)
-// upload 
-app.post('/upload',function(req,res){
-	// isUserAutenticated,
-	upload(req,res,function(err){
-		if (err) {
-			return res.end('Error uploading file.')	
-		}
-		res.end("File is uploaded")
-	})
-})
-// delete file uploaded function
-	app.post('/upload/delete',function(req,res){
-		try {
-		const fs = require('fs')
-		console.log(req.body.filename)
-		fs.unlink(process.cwd() + '/public/uploads/' + req.body.filename,(err)=>{
-		if (err) throw err; {
-			res.send("Delete sucessful")
-			// console.log('successfully deleted material file');
-		}
-	})
-	} catch(err) {
-			res.status(500).send({
-	    	error: 'An error has occured trying to delete file the material'
-	  	})
-	}
-})
+  let storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+      callback(null, "./public/uploads");
+    },
+    filename: function(req, file, callback) {
+      // callback(null, file.fieldname + '-' + Date.now());
+      console.log(file);
+      callback(null, file.originalname);
+    }
+  })
+  let upload = multer({ storage: storage }).array("userPhoto", 10)
+
+  // upload
+  app.post("/upload", function(req, res) {
+    // isUserAuthenticated,
+    upload(req, res, function(err) {
+      if (err) {
+        return res.end("Error uploading file.");
+
+        }
+        res.end("File is uploaded");
+      })
+  })
+
+
+  //delete file uploaded function
+  app.post('/upload/delete', async function (req, res) {
+    try {
+      const fs = require('fs');      
+
+      console.log(req.body.filename)
+      fs.unlink(process.cwd() + '/public/uploads/' + req.body.filename, (err) => {
+        if (err) throw err;
+        res.send("Delete sucessful")
+        // console.log('successfully deleted material file');
+      });
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to delete file the material'
+      })
+    }
+  }),
 
 	// RestFul Api for users menagement
 	app.get('/users',
