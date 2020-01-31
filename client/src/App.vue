d042489a823be16323e912114151ca4e05a06bc2
pakawat <pakawatkut@gmail.com>
2020-01-31 07:49:38 +0700

    Font-end edit blog and upload pictures

--- client/src/App.vue
+++ client/src/App.vue
@@ -21,4 +21,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+/* Fade transition */
+.fade-enter, .fade-leave-to {
+    opacity: 0;
+}
+.fade-enter-active, .fade-leave-active {    
+    transition: opacity 0.5s;
+}
+.fade-enter-to {
+    opacity: 1;
+}
 </style>
